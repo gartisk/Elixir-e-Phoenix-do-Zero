f978cc1496a81014f42264c47e49dfdedef76f9c
Guilherme Araujo <gartisk@gmail.com>
2020-12-18 19:41:44 -0300

    3:37 Criando o turno do computador

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -5,6 +5,7 @@ defmodule ExMon do
   alias ExMon.Game.{Actions, Status}
 
   @computer_name "Robotinik"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
@@ -22,6 +23,8 @@ defmodule ExMon do
     move
     |> Actions.fetch_move()
     |> do_move()
+
+    computer_move(Game.info())
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
@@ -35,4 +38,11 @@ defmodule ExMon do
     Status.print_round_message(Game.info())
   end
 
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move = {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+
+  end
+
+  defp computer_move(_), do: :ok
 end
