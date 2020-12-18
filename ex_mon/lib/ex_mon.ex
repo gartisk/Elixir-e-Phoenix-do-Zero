4003962f0f475b4fb48476446ef74eb6e64aac5e
Guilherme Araujo <gartisk@gmail.com>
2020-12-18 00:57:00 -0300

    3:30 Selecionando o movimento de cura e ataque

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -20,6 +20,17 @@ defmodule ExMon do
 
   def make_move(move) do
     move
-    |> Actions.fetch_move
+    |> Actions.fetch_move()
+    |> do_move()
   end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> "realiza_cura"
+      move -> Actions.attack(move)
+    end
+  end
+
 end
--- ex_mon/lib/game/status.ex
+++ ex_mon/lib/game/status.ex
@@ -5,4 +5,8 @@ defmodule ExMon.Game.Status do
     IO.inspect(Game.info())
     IO.puts("-------------------------")
   end
+
+  def print_wrong_move_message(move) do
+    IO.puts("\n===== Invalid move: #{move} =====\n")
+  end
 end
