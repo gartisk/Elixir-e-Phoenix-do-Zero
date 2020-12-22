17c8eb48a5b81cd7d719cb6e4db42b08d27e2a3a
Guilherme Araujo <gartisk@gmail.com>
2020-12-22 01:00:43 -0300

    4.*46 Estrut basica dos diretorios - API Iniciada

---
+++ web/ex_mon/config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :ex_mon,
+  ecto_repos: [ExMon.Repo]
+
+# Configures the endpoint
+config :ex_mon, ExMonWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "iiUofwC6BvsqG4gvyCFDc2S2ck2W3p/AHlQBBEzR4ZQW+I4skEw64UOq73bE4N6T",
+  render_errors: [view: ExMonWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: ExMon.PubSub,
+  live_view: [signing_salt: "5r1DP3Rh"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
