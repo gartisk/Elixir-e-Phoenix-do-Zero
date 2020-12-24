60db7ac7ae811ee63cc23b4c75c30602a7acfb5c
Guilherme Araujo <gartisk@gmail.com>
2020-12-24 20:01:20 -0300

    4.53 O módulo Create Trainer

--- web/ex_mon/lib/ex_mon.ex
+++ web/ex_mon/lib/ex_mon.ex
@@ -1,9 +1,5 @@
 defmodule ExMon do
-  @moduledoc """
-  ExMon keeps the contexts that define your domain
-  and business logic.
+  alias ExMon.Trainer
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate create_trainer(params), to: Trainer.Create, as: :call
 end
