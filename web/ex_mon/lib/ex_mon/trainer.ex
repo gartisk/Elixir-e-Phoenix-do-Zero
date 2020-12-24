7e94a9d6847c73923c5c86f4922c59038315a7d0
Guilherme Araujo <gartisk@gmail.com>
2020-12-24 19:16:09 -0300

    4:51 Encriptando o password do Trainer - Ajuste

--- web/ex_mon/lib/ex_mon/trainer.ex
+++ web/ex_mon/lib/ex_mon/trainer.ex
@@ -24,4 +24,6 @@ defmodule ExMon.Trainer do
   defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
     change(changeset, Pbkdf2.add_hash(password))
   end
+
+  defp put_pass_hash(changeset), do: changeset
 end
