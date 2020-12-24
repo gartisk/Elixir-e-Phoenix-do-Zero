c6eee801892ba76c988410212f32b55d4af99b36
Guilherme Araujo <gartisk@gmail.com>
2020-12-24 00:02:41 -0300

    4:50 Entendendo o que são os changesets

--- web/ex_mon/lib/ex_mon/trainer.ex
+++ web/ex_mon/lib/ex_mon/trainer.ex
@@ -15,5 +15,6 @@ defmodule ExMon.Trainer do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
+    |> validate_length(:password_hash, min: 6)
   end
 end
