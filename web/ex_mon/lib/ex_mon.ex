bf15fedefce183e2f36ce9e1ed3f19e14a1933aa
Guilherme Araujo <gartisk@gmail.com>
2020-12-25 13:58:15 -0300

    4.62 Criando a lógica do Update

--- web/ex_mon/lib/ex_mon.ex
+++ web/ex_mon/lib/ex_mon.ex
@@ -4,4 +4,5 @@ defmodule ExMon do
   defdelegate create_trainer(params), to: Trainer.Create, as: :call
   defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
   defdelegate fetch_trainer(params), to: Trainer.Get, as: :call
+  defdelegate update_trainer(params), to: Trainer.Update, as: :call
 end
