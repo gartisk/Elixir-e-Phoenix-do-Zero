c3de4504237b49cd65f47f7868e9cee192b98b59
Guilherme Araujo <gartisk@gmail.com>
2020-12-24 23:03:55 -0300

    4.59 Criando o GET trainer

--- web/ex_mon/lib/ex_mon.ex
+++ web/ex_mon/lib/ex_mon.ex
@@ -3,4 +3,5 @@ defmodule ExMon do
 
   defdelegate create_trainer(params), to: Trainer.Create, as: :call
   defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
+  defdelegate fetch_trainer(params), to: Trainer.Get, as: :call
 end
---
+++ web/ex_mon/lib/ex_mon/trainer/get.ex
@@ -0,0 +1,20 @@
+defmodule ExMon.Trainer.Get do
+  alias ExMon.{Repo, Trainer}
+  alias Ecto.UUID
+
+  def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, uuid} -> get(uuid)
+    end
+
+  end
+
+  defp get(uuid) do
+    case Repo.get(Trainer, uuid) do
+      nil -> {:error, "Trainer not found!"}
+      trainer -> {:ok, trainer}
+    end
+  end
+
+end
