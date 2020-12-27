a2b54d6ba0f58531e013136b50f938c0eb6f12d1
Guilherme Araujo <gartisk@gmail.com>
2020-12-26 22:32:23 -0300

    4.79  Testando o schema do Trainer

--- web/ex_mon/config/test.exs
+++ web/ex_mon/config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :ex_mon, ExMon.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "admin",
   database: "ex_mon_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
