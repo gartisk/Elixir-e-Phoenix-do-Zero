8c3c8a0bffd254b0b3effd7f19e596fd1afb7979
Guilherme Araujo <gartisk@gmail.com>
2020-12-28 17:20:13 -0300

    5.86 Autenticando o Trainer via senha

--- web/ex_mon/lib/ex_mon_web/auth/guardian.ex
+++ web/ex_mon/lib/ex_mon_web/auth/guardian.ex
@@ -1,6 +1,8 @@
 defmodule ExMonWeb.Auth.Guardian do
   use Guardian, otp_app: :ex_mon
 
+  alias ExMon.{Repo, Trainer}
+
   def subject_for_token(trainer, _claims) do
     sub = to_string(trainer.id)
     {:ok, sub}
@@ -10,5 +12,24 @@ defmodule ExMonWeb.Auth.Guardian do
     claims
     |> Map.get("sub")
     |> ExMon.fetch_trainer()
+  end
+
+  def authenticate(%{"id" => trainer_id, "password" => password}) do
+    case Repo.get(Trainer, trainer_id) do
+      nil -> {:error, "Trainer not found!"}
+      trainer -> validate_password(trainer, password)
+    end
+  end
+
+  def validate_password(%Trainer{password_hash: hash} = trainer, password) do
+    case Pbkdf2.verify_pass(password, hash) do
+      true -> create_token(trainer)
+      false -> {:error, :unauthorized}
+    end
+  end
+
+  defp create_token(trainer) do
+    {:ok, token, _claims} = encode_and_sign(trainer)
+    {:ok, token}
   end
 end
