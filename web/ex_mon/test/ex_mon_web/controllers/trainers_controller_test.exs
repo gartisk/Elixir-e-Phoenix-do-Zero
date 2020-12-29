8b1d3de34c1c934fd24da35ac6ff08228bd6c19f
Guilherme Araujo <gartisk@gmail.com>
2020-12-28 22:42:12 -0300

    5.89 Corrigindo nossos testes

--- web/ex_mon/test/ex_mon_web/controllers/trainers_controller_test.exs
+++ web/ex_mon/test/ex_mon_web/controllers/trainers_controller_test.exs
@@ -1,9 +1,19 @@
 defmodule ExMonWeb.Controllers.TrainersControllerTest do
   use ExMonWeb.ConnCase
+  import ExMonWeb.Auth.Guardian
 
   alias ExMon.Trainer
 
   describe "show/2" do
+    setup %{conn: conn} do
+      params = %{name: "Gui", password: "123456"}
+      {:ok, trainer} = ExMon.create_trainer(params)
+      {:ok, token, _claims} = encode_and_sign(trainer)
+
+      conn = put_req_header(conn, "authorization", "Bearer #{token}")
+      {:ok, conn: conn}
+    end
+
     test "when there is a trainer with the given id, returns the trainer", %{conn: conn} do
       params = %{name: "Gui", password: "123456"}
 
